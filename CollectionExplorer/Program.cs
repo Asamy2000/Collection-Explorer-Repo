d3a1fb2a88b89222ca12d4d41fe6f477124d26de
Asamy2000 <samy07002@gmail.com>
2023-10-19 08:49:28 +0300

    change in program.cs

--- CollectionExplorer/Program.cs
+++ CollectionExplorer/Program.cs
@@ -6,13 +6,7 @@ namespace CollectionExplorer
     {
         static void Main(string[] args)
         {
-            string str1 = "hello";
-            string str2 = "hello";
-            //bool areEqualReference = (str1 == str2);
-            Console.WriteLine(str1.GetHashCode());
-            Console.WriteLine(str2.GetHashCode());
-            //Console.WriteLine(areEqualReference);
-
+            
         }
     }
 }
